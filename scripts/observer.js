52305753cf305b7090bac0ef520d8e118d880c12
Moritz Bützer <moritz@buetzer.bz>
2018-12-11 11:01:59 +0100

    multi devices

--- scripts/observer.js
+++ scripts/observer.js
@@ -1,31 +1,38 @@
-const request = require('request');
-const events = require('events');
-const config = require('config');
+const request = require('request')
+const events = require('events')
+const config = require('config')
 
-var api_url = 'https://mystrom.ch/mobile/device';
-var eventEmitter = new events.EventEmitter();
-var lastStatus = '';
+var api_url = 'https://mystrom.ch/mobile/device'
+var eventEmitter = new events.EventEmitter()
 
-var device = config.get('devices')[0];
+var devices = config.get('devices');
 
 eventEmitter.on('newStatus', function(status, device) {
   console.log(new Date().toString() +' ['+device.title+']: '+status);
 });
 
-const intervalObj = setInterval(() => {
-  request(api_url, { 
-    json: true,
-    qs: {
-      'authToken': config.get('api.authToken'),
-      'id': device.deviceid
-    }
-  }, (err, res, body) => {
-    if (err) { return console.log(err); }
-    device.mappings.forEach(function(mapping) {
-      if( (Math.abs(body.device.energyReport.power - mapping.value) <= mapping.tolerance) && (lastStatus != mapping.status) ) {
-        eventEmitter.emit('newStatus', mapping.status, device);
-        lastStatus = mapping.status;
+devices.forEach(function(device){
+  createInterval(device)
+})
+
+function createInterval(device)
+{
+  let lastStatus = ''
+  setInterval(() => {
+    request(api_url, { 
+      json: true,
+      qs: {
+        'authToken': config.get('api.authToken'),
+        'id': device.deviceid
       }
-    })
-  });
-}, config.get('interval'));
+    }, (err, res, body) => {
+      if (err) { return console.log(err); }
+      device.mappings.forEach(function(mapping) {
+        if( (Math.abs(body.device.energyReport.power - mapping.value) <= mapping.tolerance) && (lastStatus != mapping.status) ) {
+          eventEmitter.emit('newStatus', mapping.status, device)
+          lastStatus = mapping.status
+        }
+      })
+    });
+  }, config.get('interval'))
+}
